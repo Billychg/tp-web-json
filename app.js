a8d8a83bde2e6276c50c38c0150ffc5dd75db18a
Billychg <Billychg4@gmail.com>
2025-11-28 14:03:21 +0100

    Final commit - TP Web JSON

--- app.js
+++ app.js
@@ -4,8 +4,61 @@ fetch("./data.json")
     const list = document.getElementById("students");
     data.students.forEach((student) => {
       const li = document.createElement("li");
-      li.textContent = `${student.name} — Score: ${student.score}`;
+      li.textContent = `${student.name} — Classe: ${student.class} — Score: ${student.score}`;
       list.appendChild(li);
     });
   })
   .catch((error) => console.error("Error loading JSON:", error));
+
+function addname(){
+    let addname = document.getElementById("addname");
+    let valeur = addname.value;
+    return valeur;
+}
+
+function addscore(){
+    let addscore = document.getElementById("addscore");
+    let valeur = addscore.value;
+    return valeur;
+}
+
+function addclasse(){
+    let addclass = document.getElementById("addclass");
+    let valeur = addclass.value;
+    return valeur;
+}
+
+function addstudent(){
+    let score = addscore();
+    let name = addname();
+    let classe = addclasse();
+
+    const list = document.getElementById("students");
+    const li = document.createElement("li");
+
+    if (name === ""){
+        name = "Inconnu";
+    }
+
+    if (score === ""){
+        score = "Inconnu";
+    }
+
+    if (classe === ""){
+        classe = "Inconnu";
+    }
+
+    li.textContent = name + " — Classe: " + classe + " — Score: " + score;
+    list.appendChild(li);
+
+    document.getElementById("addname").value = "";
+    document.getElementById("addclass").value = "";
+    document.getElementById("addscore").value = "";
+}
+
+function removeline(){
+    const list = document.getElementById("students");
+    if (list.lastChild) {
+        list.removeChild(list.lastChild);
+    }
+}
